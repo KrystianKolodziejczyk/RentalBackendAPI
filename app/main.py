14f9b1c7f88aa3bcc4520f05c94416bd4747d51a
Krystian Kolodziejczyk <krystiankolodziejczyk2004@gmail.com>
2026-01-05 10:11:01 +0100

    Prepared project for DDD infrastructure

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from uuid import uuid4
+from enum import Enum
 
 app = FastAPI()
 
 
+class RentStatus(Enum):
+    RENTED = "rented"
+    AVAILABLE = "available"
+
+
 # Cars class
 class Car:
+    id: str
     brand: str
     model: str
     year: int
@@ -14,34 +22,36 @@ class Car:
         self.brand = brand
         self.model = model
         self.year = year
+        self.id = uuid4()
 
 
-# Store Item class
-class StoreItem(Car):
-    __id: int
+# Rented item class
+class StoreItem:
+    car: Car
+    status: RentStatus
 
-    def __init__(self, brand: str, model: str, year: int, id: int) -> None:
-        super().__init__(brand=brand, model=model, year=year)
-        self.__id = id
-
-    @property
-    def id(self):
-        return self.__id
+    def __init__(self, car: Car, status: RentStatus):
+        self.car = car
+        self.status = status
 
 
 # Storehouse Class
 class Storehouse:
-    possesedCars: list
+    ownedCars: list[StoreItem]
 
     def __init__(self) -> None:
-        self.possesedCars = []
+        self.ownedCars = []
 
+    def add_car(self, car: Car):
+        self.ownedCars.append(StoreItem(car=car, status=RentStatus.AVAILABLE))
 
-# Rentend cars class
-class Rented:
-    def __init__(self, id: int, status: str):
-        self.id = id
-        self.status = status
+    @property
+    def qty(self):
+        return len(self.ownedCars)
+
+    @property
+    def get_available(self):
+        return [item for item in self.ownedCars if item.status == RentStatus.AVAILABLE]
 
 
 # Get response model
@@ -55,36 +65,33 @@ class GetCarResponse(BaseModel):
 # Signletone instance
 storehouse = Storehouse()
 
-# Fake Database
-storehouse.possesedCars = [
-    StoreItem(id=1, brand="BMW", model="M3", year=2019),
-    StoreItem(id=2, brand="BMW", model="M3", year=2019),
-    StoreItem(id=3, brand="BMW", model="M3", year=2019),
-    StoreItem(id=4, brand="BMW", model="M3", year=2019),
-    StoreItem(id=5, brand="BMW", model="M4", year=2019),
-    StoreItem(id=6, brand="BMW", model="M4", year=2019),
-    StoreItem(id=7, brand="BMW", model="M4", year=2019),
-    StoreItem(id=8, brand="BMW", model="M4", year=2019),
-    StoreItem(id=9, brand="Audi", model="RS6", year=2019),
-    StoreItem(id=10, brand="Audi", model="RS6", year=2019),
-    StoreItem(id=11, brand="Audi", model="RS6", year=2019),
-    StoreItem(id=12, brand="Audi", model="RS7", year=2019),
-    StoreItem(id=13, brand="Audi", model="RS7", year=2019),
-]
+car1 = Car(brand="BMW", model="M3", year=2022)
+car3 = Car(brand="BMW", model="M3", year=2022)
+car4 = Car(brand="BMW", model="M3", year=2022)
+car6 = Car(brand="BMW", model="M3", year=2022)
+car7 = Car(brand="BMW", model="M3", year=2022)
+car18 = Car(brand="BMW", model="M3", year=2022)
+car9 = Car(brand="BMW", model="M3", year=2022)
+storehouse.add_car(car1)
+storehouse.add_car(car3)
+storehouse.add_car(car4)
+storehouse.add_car(car6)
+storehouse.add_car(car7)
+storehouse.add_car(car18)
+storehouse.add_car(car9)
+print(storehouse.get_available)
 
 
 # Gets all cars
 @app.get("/api/cars", status_code=200, response_model=list[GetCarResponse])
 async def get_all_cars() -> list[StoreItem]:
-    return storehouse.possesedCars
+    pass
 
 
 # Gets one car by ID
 @app.get("/api/cars/{car_id}", status_code=200, response_model=GetCarResponse)
 async def get_one_car(car_id: int):
-    for car in storehouse.possesedCars:
-        if car.id == car_id:
-            return car
+    pass
 
 
 # Adds new car
